1ae5afd1c299c933ddf4bab0a678f781dd9b0ffc
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-07 08:35:14 +0100

    commit faqs view model

---
+++ lib/ui/views/faqs/faqs_viewmodel.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/animation.dart';
+import 'package:goasbar/app/app.locator.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class FAQsViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+
+  void back() {
+    _navigationService.back();
+  }
+
+  void navigateTo ({view}) {
+    _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
+  }
+}
