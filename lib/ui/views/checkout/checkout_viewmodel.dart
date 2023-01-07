e8fad899df5cda951bcbda76d3bfd31127e36413
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-07 08:31:36 +0100

    commit checkout view model

---
+++ lib/ui/views/checkout/checkout_viewmodel.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:goasbar/app/app.locator.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class CheckoutViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+
+  void navigateTo({view}) {
+    _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
+  }
+
+  void back() {
+    _navigationService.back();
+  }
+}
