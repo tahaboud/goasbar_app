6f44b7c530fb50ee550c7bd293e3d6b68cc6a499
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-07 08:31:44 +0100

    commit chip widget

---
+++ lib/ui/widgets/chip_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:goasbar/ui/views/messages_notifications/messages_notifications_viewmodel.dart';
+import 'package:styled_widget/styled_widget.dart';
+
+class ChipWidget extends StatelessWidget {
+  const ChipWidget({
+    Key? key,
+    this.model,
+    this.index,
+  }) : super(key: key);
+  final MessagesNotificationsViewModel? model;
+  final int? index;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 30,
+      width: 100,
+      child: Text(index! == 1 ? "Messages" : "Notifications").center(),
+    ).decorated(
+      borderRadius: BorderRadius.circular(20),
+      color: model!.indexTab == index ? const Color(0xffF9F9F9) : const Color(0xffE8E9ED),
+      animate: true,
+    ).animate(const Duration(milliseconds: 300), Curves.easeIn)
+        .gestures(onTap: () => model!.changeTab(index!));
+  }
+}
