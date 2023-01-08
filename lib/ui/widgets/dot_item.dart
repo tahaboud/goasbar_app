61b7a59e04b82b6e765fafb071c8762d5023ac64
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-08 08:23:04 +0100

    commit dot item updates

--- lib/ui/widgets/dot_item.dart
+++ lib/ui/widgets/dot_item.dart
@@ -4,13 +4,15 @@ import 'package:styled_widget/styled_widget.dart';
 class DotItem extends StatelessWidget {
   const DotItem({
     Key? key,
-    this.condition
+    this.condition,
+    this.color = Colors.white,
   }) : super(key: key);
   final bool? condition;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
-    return Container(decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(10)),)
+    return Container(decoration: BoxDecoration(color: color, borderRadius: BorderRadius.circular(10)),)
         .height(8)
         .width(8)
         .opacity(condition! ? 1 : 0.6, animate: true)
