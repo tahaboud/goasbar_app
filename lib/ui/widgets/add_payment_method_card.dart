207f3212fc6d34dcc13bc3e576e2337036dd2ced
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-08 20:34:38 +0100

    commit add payment method card

---
+++ lib/ui/widgets/add_payment_method_card.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:goasbar/shared/colors.dart';
+import 'package:styled_widget/styled_widget.dart';
+
+class AddPaymentMethodCard extends StatelessWidget {
+  const AddPaymentMethodCard({
+    Key? key,
+    this.image,
+    this.isSelected,
+    this.onTap,
+  }) : super(key: key);
+  final String? image;
+  final bool? isSelected;
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(width: isSelected! ? 2 : 1, color: isSelected! ? kMainColor2.withOpacity(0.8) : kGrayText,),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Image.asset('assets/icons/$image.png').center(),
+    ).gestures(onTap: onTap);
+  }
+}
