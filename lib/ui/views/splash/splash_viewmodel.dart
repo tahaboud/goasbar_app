a31eb9cc3c1e28d7ec96ac1698ba735e7cc08f74
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-08 08:24:28 +0100

    commit splash view model updates

--- lib/ui/views/splash/splash_viewmodel.dart
+++ lib/ui/views/splash/splash_viewmodel.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
 import 'package:flutter/animation.dart';
 import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/services/token_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class SplashViewModel extends BaseViewModel {
   bool isDone = false;
   final _navigationService = locator<NavigationService>();
+  final _tokenService = locator<TokenService>();
 
   startAnimation() async {
     Timer(const Duration(milliseconds: 1000), () async { isDone = true; notifyListeners(); },);
@@ -14,5 +16,9 @@ class SplashViewModel extends BaseViewModel {
 
   void navigateTo({view}) {
     _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
+  }
+
+  checkToken () async {
+    return await _tokenService.isTokenExist();
   }
 }
