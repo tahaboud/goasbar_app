d4f911933d1472aa0ef0fd36b40fc1889b6318ae
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-08 08:23:38 +0100

    commit guest view model updates

--- lib/ui/views/guest/guest_viewmodel.dart
+++ lib/ui/views/guest/guest_viewmodel.dart
@@ -1,5 +1,16 @@
+import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/services/token_service.dart';
 import 'package:stacked/stacked.dart';
 
 class GuestViewModel extends IndexTrackingViewModel {
+  final _tokenService = locator<TokenService>();
+  bool? isTokenExist;
 
+  checkToken () async {
+    setBusy(true);
+    isTokenExist = await _tokenService.isTokenExist();
+    setBusy(false);
+    notifyListeners();
+    return isTokenExist;
+  }
 }
