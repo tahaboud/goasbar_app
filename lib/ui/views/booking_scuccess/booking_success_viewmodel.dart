72b31f7cb8ffe940b0704d017fae3fa91c2e838f
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-08 08:21:56 +0100

    commit booking access updates

--- lib/ui/views/booking_scuccess/booking_success_viewmodel.dart
+++ lib/ui/views/booking_scuccess/booking_success_viewmodel.dart
@@ -10,12 +10,6 @@ import 'package:stacked_services/stacked_services.dart';
 
 class BookingSuccessViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
-  final _dialogService = locator<DialogService>();
-  final _validationService = locator<ValidationService>();
-  int? selectedIndexDate;
-  int? selectedIndexTime;
-  int? numberOfGuests = 0;
-  TextEditingController birthDate = TextEditingController();
 
   void navigateTo({view}) {
     _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
@@ -23,66 +17,5 @@ class BookingSuccessViewModel extends BaseViewModel {
 
   void back() {
     _navigationService.back();
-  }
-
-  void changeSelectionDate({int? index}) {
-    if (selectedIndexDate == index) {
-      selectedIndexDate = null;
-    } else {
-      selectedIndexDate = index;
-    }
-    notifyListeners();
-  }
-
-  void changeSelectionTime({int? index}) {
-    if (selectedIndexTime == index) {
-      selectedIndexTime = null;
-    } else {
-      selectedIndexTime = index;
-    }
-    notifyListeners();
-  }
-
-  void incDecrease({int? value}) {
-    if (numberOfGuests! > 0 || value! > 0) {
-      numberOfGuests = numberOfGuests! + value!;
-    }
-    notifyListeners();
-  }
-
-  void showAvailablePlacesDialog({gen}) {
-    _dialogService.showCustomDialog(
-      variant: DialogType.availablePlaces,
-    );
-  }
-
-  void showBirthDayPicker(context) async {
-    DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(1950),
-      lastDate: DateTime.now(),
-    );
-
-    final DateFormat formatter = DateFormat('dd . MM yyyy');
-    final String formatted = formatter.format(picked!);
-
-    birthDate.text = formatted;
-    notifyListeners();
-  }
-
-  validatePhoneNumber ({String? value}) {
-    _validationService.validatePhoneNumber(value);
-  }
-
-  void showAvailableTimingsPicker({context}) {
-    showCalendarDatePicker2Dialog(
-      context: context,
-      config: CalendarDatePicker2WithActionButtonsConfig(
-        calendarType: CalendarDatePicker2Type.range,
-        selectedDayHighlightColor: kMainColor1,
-      ),
-      dialogSize: const Size(325, 350),
-    );
   }
 }
