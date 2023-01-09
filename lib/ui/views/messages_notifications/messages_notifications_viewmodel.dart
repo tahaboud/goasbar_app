1cd80e7215949add5a29f5075fc134882aba948f
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-09 21:53:15 +0100

    commit message notifications view model

---
+++ lib/ui/views/messages_notifications/messages_notifications_viewmodel.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:goasbar/app/app.locator.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class MessagesNotificationsViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+  int indexTab = 1;
+
+  void navigateTo({view}) {
+    _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
+  }
+
+  void back() {
+    _navigationService.back();
+  }
+
+  void changeTab(int value) {
+    indexTab = value;
+    notifyListeners();
+  }
+}
