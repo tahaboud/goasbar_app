9a227bdf30a8a88b9800025c2782cfb56f98f2df
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-09 21:52:33 +0100

    commit media service

---
+++ lib/services/media_service.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+
+class MediaService {
+  final ImagePicker _picker = ImagePicker();
+  // Pick an image
+
+  Future<File?> getImage() async {
+    final XFile? pickedFile =
+    await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      return File(pickedFile.path);
+    }
+    else {
+      return null;
+    }
+  }
+}
