ea6ae72f323a3251de95e71d471ad7f7dea63c20
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-09 21:54:15 +0100

    commit notification item

---
+++ lib/ui/widgets/notification_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:goasbar/shared/colors.dart';
+import 'package:goasbar/shared/ui_helpers.dart';
+import 'package:styled_widget/styled_widget.dart';
+
+class NotificationItem extends StatelessWidget {
+  const NotificationItem({
+    Key? key,
+    this.onTap,
+  }) : super(key: key);
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset("assets/icons/logo_notification.png",),
+        horizontalSpaceSmall,
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text('We have update for you',),
+            verticalSpaceSmall,
+            Text('New experinces from Elsalam egency', style: TextStyle(color: kMainDisabledGray, fontSize: 12),)
+          ],
+        ),
+        const Spacer(),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: const [
+            Text('2 min ago', style: TextStyle(color: kMainDisabledGray,),),
+            verticalSpaceSmall,
+          ],
+        ),
+      ],
+    ).padding(bottom: 20).gestures(onTap: onTap);
+  }
+}
