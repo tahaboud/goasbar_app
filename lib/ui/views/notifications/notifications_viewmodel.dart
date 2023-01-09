efaccd819831419a42dc747b318761a143968418
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-09 21:54:31 +0100

    commit notifications view model

---
+++ lib/ui/views/notifications/notifications_viewmodel.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'package:flutter/animation.dart';
+import 'package:goasbar/app/app.locator.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class NotificationsViewModel extends BaseViewModel {
+  bool isDone = false;
+  final _navigationService = locator<NavigationService>();
+
+  startAnimation() async {
+    Timer(const Duration(milliseconds: 1000), () async { isDone = true; notifyListeners(); },);
+  }
+
+  void navigateTo({view}) {
+    _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
+  }
+
+  loadData() async {
+    setBusy(true);
+    Timer(const Duration(milliseconds: 1000), () { setBusy(false); },);
+  }
+}
