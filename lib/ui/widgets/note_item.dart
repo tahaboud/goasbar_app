891edd4ff7c6466915715004983bfc3a8cfd796e
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-09 21:53:48 +0100

    commit note item

---
+++ lib/ui/widgets/note_item.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:goasbar/shared/ui_helpers.dart';
+import 'package:goasbar/ui/widgets/note_item_dot.dart';
+import 'package:goasbar/ui/widgets/note_item_text.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({
+    Key? key,
+    this.text,
+  }) : super(key: key);
+  final String? text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        horizontalSpaceSmall,
+        const NoteItemDot(),
+        horizontalSpaceSmall,
+        NoteItemText(text: text!),
+      ],
+    );
+  }
+}
