5bb570c5ac16e7b695355027004663e3ee719a68
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-10 00:57:01 +0100

    commit reset password view model

---
+++ lib/ui/views/reset_password/reset_password_viewmodel.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/animation.dart';
+import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/services/validation_service.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class ResetPasswordViewModel extends BaseViewModel {
+  bool isObscure = true;
+  final _navigationService = locator<NavigationService>();
+  final _validationService = locator<ValidationService>();
+
+  void changeObscure() {
+    isObscure = !isObscure;
+    notifyListeners();
+  }
+
+  void back() {
+    _navigationService.back();
+  }
+
+  void navigateTo ({view}) {
+    _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
+  }
+
+  String? validatePassword ({String? value}) {
+    return _validationService.passwordValidation(value);
+  }
+
+  String? validateRePassword ({String? password, String? rePassword}) {
+    return _validationService.rePasswordValidation(password: password, rePassword: rePassword);
+  }
+}
