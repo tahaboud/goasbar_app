1b8fe366081482b3ca760f90e44d66d723d2621f
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-10 01:01:42 +0100

    commit trip card vm

---
+++ lib/ui/widgets/trip_card/trip_card_viewmodel.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/animation.dart';
+import 'package:goasbar/app/app.locator.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class TripCardViewModel extends BaseViewModel {
+  bool isFav = false;
+  final _navigationService = locator<NavigationService>();
+
+  void addFavorites() {
+    isFav = !isFav;
+    notifyListeners();
+  }
+
+  void navigateTo({view}) {
+    _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
+  }
+}
