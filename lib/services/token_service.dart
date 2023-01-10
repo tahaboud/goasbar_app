48ccabd18adaf009a145a064ff7a637858fead96
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-10 01:01:27 +0100

    commit token service

---
+++ lib/services/token_service.dart
@@ -0,0 +1,24 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TokenService {
+  Future<bool> isTokenExist() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool exist = prefs.containsKey('token');
+    return exist;
+  }
+
+  Future<void> clearToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('token');
+  }
+
+  void setTokenValue(String token) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('token', token);
+  }
+
+  Future<String> getTokenValue() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token')!;
+  }
+}
