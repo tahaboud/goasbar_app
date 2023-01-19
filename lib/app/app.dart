ae17ea788bddecb2db179dada99d3c6ac6d7ae33
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-19 12:04:23 +0100

    commit app

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,6 +4,7 @@ import 'package:goasbar/ui/views/login/login_view.dart';
 import 'package:goasbar/ui/views/splash/splash_view.dart';
 import 'package:goasbar/services/validation_service.dart';
 import 'package:goasbar/services/token_service.dart';
+import 'package:goasbar/services/url_service.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -16,6 +17,7 @@ import 'package:stacked_services/stacked_services.dart';
     LazySingleton(classType: ValidationService),
     LazySingleton(classType: MediaService),
     LazySingleton(classType: TokenService),
+    LazySingleton(classType: UrlService),
   ],
   routes: [
     MaterialRoute(
