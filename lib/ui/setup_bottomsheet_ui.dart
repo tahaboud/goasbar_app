e6ad885507cb362e77cbd7b03bddb02237d7ef76
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-19 12:07:06 +0100

    commit setup bottom sheet ui

--- lib/ui/setup_bottomsheet_ui.dart
+++ lib/ui/setup_bottomsheet_ui.dart
@@ -1,5 +1,8 @@
 import 'package:goasbar/enum/bottom_sheet_type.dart';
+import 'package:goasbar/ui/views/add_experience/add_experience_view.dart';
 import 'package:goasbar/ui/views/be_hosted/be_hosted_view.dart';
+import 'package:goasbar/ui/views/new_timing/new_timing_view.dart';
+import 'package:goasbar/ui/views/publish_success/publish_success_view.dart';
 import 'package:stacked_services/stacked_services.dart';
 import '../app/app.locator.dart';
 
@@ -9,6 +12,15 @@ void setupBottomSheetUi() {
   final builders = {
     BottomSheetType.beHosted: (context, sheetRequest, completer) =>
         BeHostedView(request: sheetRequest, completer: completer),
+
+    BottomSheetType.newTiming: (context, sheetRequest, completer) =>
+        NewTimingView(request: sheetRequest, completer: completer),
+
+    BottomSheetType.publishSuccess: (context, sheetRequest, completer) =>
+        PublishSuccessView(request: sheetRequest, completer: completer),
+
+    BottomSheetType.addExperience: (context, sheetRequest, completer) =>
+        AddExperienceView(request: sheetRequest, completer: completer),
   };
 
   bottomSheetService.setCustomSheetBuilders(builders);
