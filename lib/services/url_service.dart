406527b015568c681fdf2952f4ec0c5ac9301382
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-20 12:10:33 +0100

    commit url service

---
+++ lib/services/url_service.dart
@@ -0,0 +1,35 @@
+import 'package:url_launcher/url_launcher.dart';
+
+class UrlService {
+  String? _encodeQueryParameters(Map<String, String> params) {
+    return params.entries.map((MapEntry<String, String> e) =>
+    '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
+        .join('&');
+  }
+
+  launchEmail () async{
+    final Uri emailLaunchUri = Uri(
+      scheme: 'mailto',
+      path: 'contact@goasbar.com',
+      query: _encodeQueryParameters(<String, String>{
+        'subject': 'Support',
+      }),
+    );
+
+    launchUrl(emailLaunchUri);
+  }
+
+  launchWebsite() async {
+    if (!await launchUrl(Uri.parse("https://www.goasbar.com/"))) {
+      throw 'Could not launch https://www.goasbar.com/';
+    }
+  }
+
+  launchPhoneCall() async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: "+966 (483) -565 9898",
+    );
+    await launchUrl(launchUri);
+  }
+}
