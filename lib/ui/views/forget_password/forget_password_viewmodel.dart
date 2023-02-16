7119db6adf3d6a2412266b3f81b9803f0d4efc65
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-16 20:20:58 +0100

    commit forget password view model update

--- lib/ui/views/forget_password/forget_password_viewmodel.dart
+++ lib/ui/views/forget_password/forget_password_viewmodel.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/animation.dart';
 import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/services/auth_service.dart';
+import 'package:goasbar/services/validation_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class ForgetPasswordViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
+  final _authService = locator<AuthService>();
+  final _validationService = locator<ValidationService>();
 
   void back() {
     _navigationService.back();
@@ -12,5 +16,13 @@ class ForgetPasswordViewModel extends BaseViewModel {
 
   void navigateTo ({view}) {
     _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
+  }
+
+  Future<bool?> forgetPassword({String? phoneNumber}) async {
+    return _authService.requestResetPassword(phoneNumber: phoneNumber,);
+  }
+
+  String? validatePhoneNumber ({String? value}) {
+    return _validationService.validatePhoneNumber(value);
   }
 }
