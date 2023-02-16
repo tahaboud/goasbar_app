d90631b7757ef3a1373db2e095ea3cdec07cef88
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-16 20:18:52 +0100

    commit be hosted view model update

--- lib/ui/views/be_hosted/be_hosted_viewmodel.dart
+++ lib/ui/views/be_hosted/be_hosted_viewmodel.dart
@@ -2,22 +2,26 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/data_models/provider_model.dart';
 import 'package:goasbar/enum/dialog_type.dart';
 import 'package:goasbar/services/media_service.dart';
+import 'package:goasbar/services/provider_api_service.dart';
 import 'package:goasbar/services/token_service.dart';
 import 'package:goasbar/services/validation_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class BeHostedInfoViewModel extends BaseViewModel {
+class BeHostedInfoViewModel extends FutureViewModel<ProviderModel?> {
   final _navigationService = locator<NavigationService>();
   final _validationService = locator<ValidationService>();
   final _dialogService = locator<DialogService>();
   final _mediaService = locator<MediaService>();
+  final _providerApiService = locator<ProviderApiService>();
   final _tokenService = locator<TokenService>();
   TextEditingController typeOfIdentity = TextEditingController();
   File? file;
   int pageIndex = 1;
+  ProviderModel? provider;
 
   void navigateTo({view}) {
     _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
@@ -68,7 +72,47 @@ class BeHostedInfoViewModel extends BaseViewModel {
     );
   }
 
-  setToken ({token}) {
-    _tokenService.setTokenValue(token);
+  String? writeIbanFormat({iban}) {
+    String? temp = "";
+    var lastI = 0;
+    var i = 0;
+    for (i = 0; i < iban.length;i++) {
+      if (i % 5 == 0 && i != 0) {
+        if (temp!.isEmpty) {
+          temp = iban.substring(lastI, i);
+          lastI = i;
+        } else {
+          temp = "$temp  ${iban.substring(lastI, i)}";
+          lastI = i;
+        }
+      }
+    }
+    temp = "$temp  ${iban.substring(lastI, i)}";
+
+    return temp;
+  }
+
+  Future<ProviderModel?> beHostedProvider({Map? body}) async {
+    String? token = await _tokenService.getTokenValue();
+    provider = await _providerApiService.createProvider(token: token, body: body,);
+
+    return provider;
+  }
+
+  Future<ProviderModel?> updateProvider({Map? body}) async {
+    String? token = await _tokenService.getTokenValue();
+    provider = await _providerApiService.updateProvider(token: token, body: body,);
+
+    return provider;
+  }
+
+  @override
+  Future<ProviderModel?> futureToRun() async {
+    String? token = await _tokenService.getTokenValue();
+    provider = await _providerApiService.getProviderInfo(token: token);
+    if (provider == null) {
+      return null;
+    }
+    return provider;
   }
 }
