5c23844f8d5732ab252b26b05ff3fb794e269300
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-16 20:22:18 +0100

    commit home view update

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,22 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:goasbar/ui/views/home/home_viewmodel.dart';
+import 'package:goasbar/ui/views/must_login_first/must_login_first_view.dart';
+import 'package:goasbar/ui/views/navbar/experience/experience_view.dart';
+import 'package:goasbar/ui/views/navbar/saved_experiences/saved_experiences_view.dart';
+import 'package:goasbar/ui/views/navbar/search/search_view.dart';
+import 'package:goasbar/ui/views/navbar/settings/settings_view.dart';
+import 'package:goasbar/ui/views/navbar/trips/trips_view.dart';
 import 'package:stacked/stacked.dart';
+import 'package:animations/animations.dart';
+import 'package:goasbar/ui/widgets/loader.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({Key? key}) : super(key: key);
+  const HomeView({Key? key,}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<HomeViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text("Home", style: TextStyle(color: Colors.black),),
+        body: model.isBusy ? const Loader() : PageTransitionSwitcher(
+          duration: const Duration(milliseconds: 300),
+          reverse: model.reverse,
+          transitionBuilder: (
+              Widget child,
+              Animation<double> animation,
+              Animation<double> secondaryAnimation,
+              ) {
+            return SharedAxisTransition(
+              animation: animation,
+              secondaryAnimation: secondaryAnimation,
+              transitionType: SharedAxisTransitionType.horizontal,
+              child: child,
+            );
+          },
+          child: getViewForIndex(index: model.currentIndex, isUser: model.isTokenExist),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white,
+          currentIndex: model.currentIndex,
+          onTap: model.setIndex,
+          items: [
+            BottomNavigationBarItem(
+              label: 'Experience',
+              icon: Image.asset("assets/icons/navbar/experience.png"),
+            ),
+            BottomNavigationBarItem(
+              label: 'Search',
+              icon: Image.asset("assets/icons/navbar/search.png"),
+            ),
+            BottomNavigationBarItem(
+              label: 'Trips',
+              icon: Image.asset("assets/icons/navbar/trips.png"),
+            ),
+            BottomNavigationBarItem(
+              label: 'Saved',
+              icon: Image.asset("assets/icons/navbar/saved.png"),
+            ),
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Image.asset("assets/icons/navbar/settings.png"),
+            ),
           ],
         ),
       ),
       viewModelBuilder: () => HomeViewModel(),
+      onModelReady: (model) => model.checkToken(),
     );
+  }
+
+  Widget getViewForIndex({int? index, bool? isUser}) {
+    switch (index) {
+      case 0:
+        return ExperienceView(isUser: isUser);
+      case 1:
+        return const SearchView();
+      case 2:
+        return isUser! ? const TripsView(text: "Trips",) : const MustLoginFirstView(text: 'Trips',);
+      case 3:
+        return isUser! ? const SavedExperiencesView(text: 'Saved Experiences',) : const MustLoginFirstView(text: 'Saved Experiences',);
+      case 4:
+        return SettingsView(isUser: isUser);
+      default:
+        return Container();
+    }
   }
 }
