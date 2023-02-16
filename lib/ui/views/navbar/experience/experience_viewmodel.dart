ee5f7d947ecd9f42b7bff85b162a9a6f6c8471c6
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-16 20:20:34 +0100

    commit experience view model update

--- lib/ui/views/navbar/experience/experience_viewmodel.dart
+++ lib/ui/views/navbar/experience/experience_viewmodel.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/animation.dart';
 import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/data_models/user_model.dart';
+import 'package:goasbar/services/auth_service.dart';
+import 'package:goasbar/services/token_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class ExperienceViewModel extends BaseViewModel {
+class ExperienceViewModel extends FutureViewModel<dynamic> {
   int index = 1;
   final _navigationService = locator<NavigationService>();
+  final _authService = locator<AuthService>();
+  final _tokenService = locator<TokenService>();
   bool? isTokenExist;
+  UserModel? user;
 
   void selectCategory({ind}) {
     index = ind;
@@ -19,5 +25,19 @@ class ExperienceViewModel extends BaseViewModel {
 
   void back() {
     _navigationService.back();
+  }
+
+  @override
+  Future futureToRun() async {
+    String token = await _tokenService.getTokenValue();
+    return _authService.getUserData(token: token).then((value) {
+      if (value != null) {
+        user = value;
+        notifyListeners();
+        return user;
+      } else {
+        return null;
+      }
+    });
   }
 }
