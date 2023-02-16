b977b48288e881233d5bfbbe88085b2bf7492a6b
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-16 20:25:33 +0100

    commit splash view model update

--- lib/ui/views/splash/splash_viewmodel.dart
+++ lib/ui/views/splash/splash_viewmodel.dart
@@ -2,17 +2,14 @@ import 'dart:async';
 import 'package:flutter/animation.dart';
 import 'package:goasbar/app/app.locator.dart';
 import 'package:goasbar/services/token_service.dart';
+import 'package:goasbar/ui/views/home/home_view.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class SplashViewModel extends BaseViewModel {
+class SplashViewModel extends FutureViewModel<dynamic> {
   bool isDone = false;
   final _navigationService = locator<NavigationService>();
   final _tokenService = locator<TokenService>();
-
-  startAnimation() async {
-    Timer(const Duration(milliseconds: 1000), () async { isDone = true; notifyListeners(); },);
-  }
 
   void navigateTo({view}) {
     _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
@@ -20,5 +17,22 @@ class SplashViewModel extends BaseViewModel {
 
   checkToken () async {
     return await _tokenService.isTokenExist();
+  }
+
+  startAnimation() async {
+    Timer(const Duration(milliseconds: 1000), () async {
+      isDone = true;
+      notifyListeners();
+    },);
+  }
+
+  @override
+  Future<dynamic> futureToRun() async {
+    setBusy(true);
+    if (await checkToken()) {
+      navigateTo(view: const HomeView());
+    }
+    setBusy(false);
+    return await checkToken();
   }
 }
