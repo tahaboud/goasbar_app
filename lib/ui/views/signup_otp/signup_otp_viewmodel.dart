7a7219ea528fb6b9eae3f481e43844ec6f16d58b
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-16 20:25:00 +0100

    commit sign up view model update

--- lib/ui/views/signup_otp/signup_otp_viewmodel.dart
+++ lib/ui/views/signup_otp/signup_otp_viewmodel.dart
@@ -1,9 +1,16 @@
 import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/data_models/auth_response.dart';
+import 'package:goasbar/services/auth_service.dart';
+import 'package:goasbar/services/token_service.dart';
 import 'package:stacked/stacked.dart';
+import 'package:goasbar/enum/status_code.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class SignUpOtpViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
+  final _tokenService = locator<TokenService>();
+  final _authService = locator<AuthService>();
+  AuthResponse? authResponse;
 
   void back() {
     _navigationService.back();
@@ -11,5 +18,21 @@ class SignUpOtpViewModel extends BaseViewModel {
 
   void navigateTo({view}) {
     _navigationService.clearStackAndShowView(view,);
+  }
+
+  Future<StatusCode> register({Map? body}) async {
+    dynamic response = await _authService.register(
+      body: body,
+    );
+    if (response == null) {
+      return StatusCode.other;
+    } else if (response == StatusCode.throttled) {
+      return StatusCode.throttled;
+    } else {
+      authResponse = response;
+      _tokenService.setTokenValue(authResponse!.token!);
+      notifyListeners();
+      return StatusCode.ok;
+    }
   }
 }
