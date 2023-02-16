9935c90d830d06025b5600ba65d8455785c42974
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-16 20:24:42 +0100

    commit settings view model update

--- lib/ui/views/navbar/settings/settings_viewmodel.dart
+++ lib/ui/views/navbar/settings/settings_viewmodel.dart
@@ -2,20 +2,32 @@ import 'dart:async';
 
 import 'package:flutter/animation.dart';
 import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/data_models/provider_model.dart';
+import 'package:goasbar/data_models/user_model.dart';
 import 'package:goasbar/enum/bottom_sheet_type.dart';
+import 'package:goasbar/services/auth_service.dart';
 import 'package:goasbar/services/media_service.dart';
+import 'package:goasbar/services/token_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'dart:io';
 
-class SettingsViewModel extends BaseViewModel {
+class SettingsViewModel extends FutureViewModel<dynamic> {
   final _navigationService = locator<NavigationService>();
   final _mediaService = locator<MediaService>();
   final _bottomSheetService = locator<BottomSheetService>();
+  final _authService = locator<AuthService>();
+  final _tokenService = locator<TokenService>();
   File? file;
+  UserModel? user;
+  ProviderModel? provider;
 
   void navigateTo({view}) {
     _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
+  }
+
+  void clearAndNavigateTo({view}) {
+    _navigationService.clearStackAndShowView(view);
   }
 
   void back() {
@@ -27,7 +39,7 @@ class SettingsViewModel extends BaseViewModel {
     Timer(const Duration(milliseconds: 1000), () { setBusy(false); },);
   }
 
-  void pickImage () async {
+  Future pickImage () async {
     file = await _mediaService.getImage();
     if (file != null) {
       notifyListeners();
@@ -39,11 +51,37 @@ class SettingsViewModel extends BaseViewModel {
       variant: BottomSheetType.beHosted,
       isScrollControlled: true,
       barrierDismissible: true,
+      data: user,
     );
 
-    if (response!.confirmed) {
+    notifyListeners();
+  }
 
-      notifyListeners();
-    }
+  Future<bool> logout() async {
+    return _authService.logout(token: await _tokenService.getTokenValue());
+  }
+
+  clearToken () {
+    _tokenService.clearToken();
+  }
+
+  Future<dynamic> getUserInfo() async {
+    String token = await _tokenService.getTokenValue();
+    user = await _authService.getUserData(token: token).then((value) {
+      if (value != null) {
+        user = value;
+        notifyListeners();
+        return user;
+      } else {
+        return null;
+      }
+    });
+
+
+  }
+
+  @override
+  Future futureToRun() {
+    return getUserInfo();
   }
 }
