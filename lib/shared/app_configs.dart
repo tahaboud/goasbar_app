828759897b9e47816e150a1701b87f73b2a2aba1
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-20 23:45:21 +0100

    commit app configs

---
+++ lib/shared/app_configs.dart
@@ -0,0 +1,53 @@
+const String baseUrl = "http://192.168.1.36:8000";
+
+const cities = [
+  "RIYADH",
+  "MECCA",
+  "MEDINA",
+  "TABUK",
+  "NAJRAN",
+  "AL_QASSIM",
+  "EASTERN_PROVINCE",
+  "ASEER",
+  "HAIL",
+  "NORTHERN_BORDERS_PROVINCE",
+  "JAZAN",
+  "AL_BAHAH",
+  "AL_JOWF",
+];
+
+const categories = [
+  "LANDSCAPE",
+  "HISTORY",
+  "SPORT",
+  "LIVE_STYLE",
+  "FOOD",
+  "ART",
+  "FASHION",
+  "OTHER",
+];
+
+final List<String> shortMonths = <String>[
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+final List<String> weekDays = <String>[
+  'SU',
+  'MO',
+  'TU',
+  'WE',
+  'TH',
+  'FR',
+  'SA',
+];
