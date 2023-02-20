b192aa2b7dd4184f2a128574831d65c710353227
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-21 00:56:25 +0100

    commit saved exp view update

--- lib/ui/widgets/saved_experience_card/saved_experience_card.dart
+++ lib/ui/widgets/saved_experience_card/saved_experience_card.dart
@@ -1,7 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:goasbar/data_models/experience_response.dart';
+import 'package:goasbar/shared/app_configs.dart';
 import 'package:goasbar/shared/ui_helpers.dart';
 import 'package:goasbar/ui/views/trip_detail/trip_detail_view.dart';
 import 'package:goasbar/ui/widgets/saved_experience_card/saved_experience_card_viewmodel.dart';
@@ -11,7 +11,11 @@ import 'package:styled_widget/styled_widget.dart';
 class SavedExperience extends StatelessWidget {
   const SavedExperience({
     Key? key,
+    this.experience,
+    this.unFavorite,
   }) : super(key: key);
+  final ExperienceResults? experience;
+  final Function()? unFavorite;
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +25,19 @@ class SavedExperience extends StatelessWidget {
           children: [
             Container(
               decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.black.withOpacity(0.1),
+                    Colors.black.withOpacity(0.3),
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
                 image: DecorationImage(
-                  image: AssetImage("assets/images/img${(Random().nextInt(3) + 1)}.png"),
+                  fit: BoxFit.cover,
+                  image: experience!.profileImage != null
+                      ? NetworkImage("$baseUrl${experience!.profileImage}") as ImageProvider
+                      : const AssetImage("assets/images/image4.png"),
                 ),
                 borderRadius: BorderRadius.circular(15),
               ),
@@ -41,25 +56,25 @@ class SavedExperience extends StatelessWidget {
                     model.isFav ? CupertinoIcons.heart_fill : CupertinoIcons.heart,
                     color: model.isFav ? Colors.redAccent : Colors.black, size: 20,
                   ).center()
-                      .gestures(onTap: () => model.addFavorites())
+                      .gestures(onTap: () => model.addFavorites(unFavorite!))
               ).height(30)
                   .width(30),
             ),
             Positioned(
               bottom: 45,
               child: Row(
-                children: const [
+                children: [
                   horizontalSpaceSmall,
-                  Text('Dammam Trip', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
+                  Text(experience!.title!, style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
                 ],
               )
             ),
             Positioned(
               bottom: 20,
               child: Row(
-                children: const [
+                children: [
                   horizontalSpaceSmall,
-                  Text('76.00 SR / Person', style: TextStyle(color: Colors.white, fontSize: 10)),
+                  Text('${experience!.price!} SR / Person', style: const TextStyle(color: Colors.white, fontSize: 10)),
                 ],
               ),
             )
