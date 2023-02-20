6df35532428aee043f21115a78e0f82b572cfb33
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-20 23:47:57 +0100

    commit request password view model

---
+++ lib/ui/views/request_reset/request_password_viewmodel.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/animation.dart';
+import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/services/auth_service.dart';
+import 'package:goasbar/services/validation_service.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class RequestPasswordViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+  final _authService = locator<AuthService>();
+  final _validationService = locator<ValidationService>();
+
+  void back() {
+    _navigationService.back();
+  }
+
+  void navigateTo ({view}) {
+    _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
+  }
+
+  Future<bool?> forgetPassword({String? phoneNumber}) async {
+    return _authService.requestResetPassword(phoneNumber: phoneNumber,);
+  }
+
+  String? validatePhoneNumber ({String? value}) {
+    return _validationService.validatePhoneNumber(value);
+  }
+}
