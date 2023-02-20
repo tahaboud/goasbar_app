7c303ab213fdd22deb0204e9470b61611a88d0eb
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-21 00:57:19 +0100

    commit saved exp view model update

--- lib/ui/views/navbar/saved_experiences/saved_experiences_viewmodel.dart
+++ lib/ui/views/navbar/saved_experiences/saved_experiences_viewmodel.dart
@@ -2,17 +2,21 @@ import 'dart:async';
 
 import 'package:flutter/animation.dart';
 import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/data_models/experience_response.dart';
+import 'package:goasbar/services/auth_service.dart';
+import 'package:goasbar/services/experience_api_service.dart';
+import 'package:goasbar/services/token_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class SavedExperiencesViewModel extends BaseViewModel {
-  int index = 1;
+class SavedExperiencesViewModel extends FutureViewModel<List<ExperienceResults?>> {
   final _navigationService = locator<NavigationService>();
-
-  void selectCategory({ind}) {
-    index = ind;
-    notifyListeners();
-  }
+  final _tokenService = locator<TokenService>();
+  final _experienceApiService = locator<ExperienceApiService>();
+  final _authService = locator<AuthService>();
+  bool? isTokenExist;
+  List<ExperienceResults>? experienceResults = [];
+  List<int>? favoriteList = [];
 
   void navigateTo({view}) {
     _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
@@ -22,8 +26,63 @@ class SavedExperiencesViewModel extends BaseViewModel {
     _navigationService.back();
   }
 
-  loadData() async {
+  Future<List<int>?> getUserData() async {
+    String token = await _tokenService.getTokenValue();
+    return _authService.getUserData(token: token).then((value) {
+      if (value != null) {
+        if (value.favoriteExperiences!.isEmpty) {
+          return [];
+        } else {
+          return value.favoriteExperiences;
+        }
+      } else {
+        return [];
+      }
+    });
+  }
+
+  updateUserData({int? index}) async {
+    String token = await _tokenService.getTokenValue();
+    favoriteList!.removeAt(index!);
+    print(favoriteList);
+    notifyListeners();
     setBusy(true);
-    Timer(const Duration(milliseconds: 1000), () { setBusy(false); },);
+    _authService.updateUserData(token: token, body: {
+      "favorite_experiences": favoriteList,
+    },).then((value) {
+      if (value != null) {
+        experienceResults!.removeAt(index);
+        notifyListeners();
+        setBusy(false);
+      }
+    });
+  }
+
+  Future<List<ExperienceResults?>> getFavoriteExperiences({String? query}) async {
+    favoriteList = await getUserData();
+    if (favoriteList!.isNotEmpty) {
+      String? token = await _tokenService.getTokenValue();
+      return await _experienceApiService.getPublicExperiences(token: token, query: query).then((value) {
+        if (value != null) {
+          for (var experience in value.results!) {
+            if (favoriteList!.contains(experience.id)) {
+              experienceResults!.add(experience);
+            }
+          }
+          notifyListeners();
+
+          return experienceResults!;
+        } else {
+          return [];
+        }
+      });
+    } else {
+      return [];
+    }
+  }
+
+  @override
+  Future<List<ExperienceResults?>> futureToRun() async {
+    return getFavoriteExperiences();
   }
 }
