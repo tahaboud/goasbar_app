83664d1f6820aa7197428a6d5899db6bd4909e10
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-20 23:50:02 +0100

    commit add exp view model update

--- lib/ui/views/add_experience/add_experience_viewmodel.dart
+++ lib/ui/views/add_experience/add_experience_viewmodel.dart
@@ -1,13 +1,18 @@
 import 'dart:io';
 
+import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/data_models/experience_response.dart';
 import 'package:goasbar/enum/bottom_sheet_type.dart';
 import 'package:goasbar/enum/dialog_type.dart';
+import 'package:goasbar/services/experience_api_service.dart';
 import 'package:goasbar/services/media_service.dart';
+import 'package:goasbar/services/timing_api_service.dart';
+import 'package:goasbar/services/token_service.dart';
 import 'package:goasbar/services/validation_service.dart';
-import 'package:goasbar/shared/ui_helpers.dart';
+import 'package:goasbar/shared/colors.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -16,100 +21,61 @@ class AddExperienceInfoViewModel extends BaseViewModel {
   final _validationService = locator<ValidationService>();
   final _dialogService = locator<DialogService>();
   final _bottomSheetService = locator<BottomSheetService>();
+  final _tokenService = locator<TokenService>();
+  final _experienceApiService = locator<ExperienceApiService>();
+  final _timingApiService = locator<TimingApiService>();
   final _mediaService = locator<MediaService>();
-  TextEditingController genderConstraints = TextEditingController();
+  TextEditingController genderConstraints = TextEditingController(text: 'No constrains');
   TextEditingController gender = TextEditingController();
-  File? file;
+  File? mainImage;
+  List<File?>? additionalImages = [];
   int? images = 0;
   int pageIndex = 1;
-  String? city;
+  int? addedProviding = 0;
+  int? addedRequirements = 0;
+  String? city = "RIYADH";
+  String? selectedExperienceCategory;
+  String? providedGoodsText = '';
+  String? requirementsText = '';
+  String? pickedTimeForRequest;
   TextEditingController startDate = TextEditingController();
   TextEditingController startTime = TextEditingController();
   TextEditingController typeOfIdentity = TextEditingController();
+  TextEditingController requirementsController1 = TextEditingController();
+  TextEditingController requirementsController2 = TextEditingController();
+  TextEditingController requirementsController3 = TextEditingController();
+  TextEditingController requirementsController4 = TextEditingController();
+  List<TextEditingController> addedRequirementsControllers = [];
+  TextEditingController providedGoodsController1 = TextEditingController();
+  TextEditingController providedGoodsController2 = TextEditingController();
+  TextEditingController providedGoodsController3 = TextEditingController();
+  TextEditingController providedGoodsController4 = TextEditingController();
+  List<TextEditingController> addedProvidedGoodsControllers = [];
 
-  List<Widget> providings = [
-    verticalSpaceSmall,
-    const TextField(
-      decoration: InputDecoration(
-        hintText: "Providing's add-ons",
-        hintStyle: TextStyle(fontSize: 14),
-      ),
-    ),
-    verticalSpaceSmall,
-    const TextField(
-      decoration: InputDecoration(
-        hintText: "Providing's add-ons",
-        hintStyle: TextStyle(fontSize: 14),
-      ),
-    ),
-    verticalSpaceSmall,
-    const TextField(
-      decoration: InputDecoration(
-        hintText: "Providing's add-ons",
-        hintStyle: TextStyle(fontSize: 14),
-      ),
-    ),
-    verticalSpaceSmall,
-    const TextField(
-      decoration: InputDecoration(
-        hintText: "Providing's add-ons",
-        hintStyle: TextStyle(fontSize: 14),
-      ),
-    ),
-  ];
+  updateSelectedExperienceCategory({String? category}) {
+    selectedExperienceCategory = category;
+    notifyListeners();
+  }
 
-  List<Widget> requirements = [
-    verticalSpaceSmall,
-    const TextField(
-      decoration: InputDecoration(
-        hintText: "Providing's add-ons",
-        hintStyle: TextStyle(fontSize: 14),
-      ),
-    ),
-    verticalSpaceSmall,
-    const TextField(
-      decoration: InputDecoration(
-        hintText: "Providing's add-ons",
-        hintStyle: TextStyle(fontSize: 14),
-      ),
-    ),
-    verticalSpaceSmall,
-    const TextField(
-      decoration: InputDecoration(
-        hintText: "Providing's add-ons",
-        hintStyle: TextStyle(fontSize: 14),
-      ),
-    ),
-    verticalSpaceSmall,
-    const TextField(
-      decoration: InputDecoration(
-        hintText: "Providing's add-ons",
-        hintStyle: TextStyle(fontSize: 14),
-      ),
-    ),
-  ];
+  void updateProvidedGoodsText ({String? text}) {
+    providedGoodsText = text;
+    notifyListeners();
+  }
+
+  void updateRequirementsText ({String? text}) {
+    providedGoodsText = text;
+    notifyListeners();
+  }
 
   addRequirements() {
-    requirements.add(verticalSpaceSmall);
-    requirements.add(const TextField(
-      decoration: InputDecoration(
-        hintText: "Providing's add-ons",
-        hintStyle: TextStyle(fontSize: 14),
-      ),
-    ));
-
+    addedRequirements = addedRequirements! + 1;
+    addedRequirementsControllers.add(TextEditingController());
     notifyListeners();
   }
 
   addProvidings() {
-    providings.add(verticalSpaceSmall);
-    providings.add(const TextField(
-      decoration: InputDecoration(
-        hintText: "Providing's add-ons",
-        hintStyle: TextStyle(fontSize: 14),
-      ),
-    ));
-
+    addedProviding = addedProviding! + 1;
+    addedProvidedGoodsControllers.add(TextEditingController());
     notifyListeners();
   }
 
@@ -154,11 +120,22 @@ class AddExperienceInfoViewModel extends BaseViewModel {
     notifyListeners();
   }
 
+  void pickMainImage () async {
+    mainImage = await _mediaService.getImage();
+    if (mainImage != null) {
+      notifyListeners();
+    }
+  }
+
   void pickImage () async {
+    File? file;
     file = await _mediaService.getImage();
     if (file != null) {
-      images = images! + 1;
-      notifyListeners();
+      if (images! < 8) {
+        images = images! + 1;
+        additionalImages!.add(file);
+        notifyListeners();
+      }
     }
   }
 
@@ -179,15 +156,23 @@ class AddExperienceInfoViewModel extends BaseViewModel {
   }
 
   void showStartDatePicker(context) async {
-    DateTime? picked = await showDatePicker(
+    List<DateTime?>? picked = await showCalendarDatePicker2Dialog(
       context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(1950),
-      lastDate: DateTime.now(),
+      config: CalendarDatePicker2WithActionButtonsConfig(
+          disableYearPicker: true,
+          firstDate: DateTime.now(),
+          calendarType: CalendarDatePicker2Type.single,
+          selectedDayHighlightColor: kMainColor1,
+      ),
+      initialValue: [
+        DateTime.now(),
+      ],
+
+      dialogSize: const Size(325, 340),
     );
 
-    final DateFormat formatter = DateFormat('dd . MM yyyy');
-    final String formatted = formatter.format(picked!);
+    final DateFormat formatter = DateFormat('yyyy-MM-dd');
+    final String formatted = formatter.format(picked![0]!);
 
     startDate.text = formatted;
     notifyListeners();
@@ -204,14 +189,25 @@ class AddExperienceInfoViewModel extends BaseViewModel {
     String minute = picked.minute < 10 ? "0${picked.minute}" : "${picked.minute}";
 
     startTime.text = "$hour:$minute $dayPeriod";
+
+    if (picked.period == DayPeriod.am) {
+      pickedTimeForRequest = "$hour:$minute";
+    } else {
+      String hourInPm = "${picked.hourOfPeriod + 12}";
+      pickedTimeForRequest = "$hourInPm:$minute";
+    }
+
     notifyListeners();
   }
 
-  showNewTimingBottomSheet() async {
+  showNewTimingBottomSheet({String? date, int? experienceId}) async {
     var response = await _bottomSheetService.showCustomSheet(
       variant: BottomSheetType.newTiming,
       isScrollControlled: true,
       barrierDismissible: true,
+      data: date,
+      // ignore: deprecated_member_use
+      customData: experienceId,
     );
 
     if (response!.confirmed) {
@@ -219,7 +215,7 @@ class AddExperienceInfoViewModel extends BaseViewModel {
     }
   }
 
-  showPublishSuccessBottomSheet() async {
+  Future showPublishSuccessBottomSheet() async {
     var response = await _bottomSheetService.showCustomSheet(
       variant: BottomSheetType.publishSuccess,
       isScrollControlled: true,
@@ -227,7 +223,19 @@ class AddExperienceInfoViewModel extends BaseViewModel {
     );
 
     if (response!.confirmed) {
-      notifyListeners();
+      return true;
     }
+  }
+
+  Future<ExperienceResults?> createExperience({Map<String, dynamic>? body}) async {
+    String? token = await _tokenService.getTokenValue();
+    return await _experienceApiService.createExperience(token: token, body: body,).then((value) {
+      if (value is ExperienceResults) {
+        _timingApiService.createTiming(token: token, experienceId: value.id);
+        return value;
+      } else {
+        return null;
+      }
+    });
   }
 }
