8ecca12abe2add5fe13b3efa7e67dd1e539405f8
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-20 23:46:25 +0100

    commit image set model

---
+++ lib/data_models/image_set_model.dart
@@ -0,0 +1,18 @@
+class ImageSet {
+  int? id;
+  String? image;
+
+  ImageSet({this.id, this.image});
+
+  ImageSet.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    image = json['image'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['image'] = image;
+    return data;
+  }
+}
