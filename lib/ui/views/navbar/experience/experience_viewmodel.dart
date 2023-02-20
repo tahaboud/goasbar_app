9644b7c59f1a937109a7a1007180e2dffdb16bcc
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-20 23:51:03 +0100

    commit exp view model update

--- lib/ui/views/navbar/experience/experience_viewmodel.dart
+++ lib/ui/views/navbar/experience/experience_viewmodel.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/animation.dart';
 import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/data_models/experience_model.dart';
+import 'package:goasbar/data_models/experience_response.dart';
 import 'package:goasbar/data_models/user_model.dart';
-import 'package:goasbar/services/auth_service.dart';
+import 'package:goasbar/services/experience_api_service.dart';
 import 'package:goasbar/services/token_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class ExperienceViewModel extends FutureViewModel<dynamic> {
+class ExperienceViewModel extends FutureViewModel<List<ExperienceResults?>> {
   int index = 1;
   final _navigationService = locator<NavigationService>();
-  final _authService = locator<AuthService>();
   final _tokenService = locator<TokenService>();
+  final _experienceApiService = locator<ExperienceApiService>();
   bool? isTokenExist;
+  ExperienceModel? experienceModels;
   UserModel? user;
 
   void selectCategory({ind}) {
     index = ind;
+    String? query;
+    if (index == 1) {
+      query = '';
+    } else if (index == 2) {
+      query = "gender=FAMILIES";
+    } else if (index == 3) {
+      query = "gender=MEN";
+    } else {
+      query = "gender=WOMEN";
+    }
+
     notifyListeners();
+
+    getPublicExperiences(query: query);
   }
 
   void navigateTo({view}) {
@@ -27,17 +43,15 @@ class ExperienceViewModel extends FutureViewModel<dynamic> {
     _navigationService.back();
   }
 
+  Future<List<ExperienceResults?>> getPublicExperiences({String? query}) async {
+    String? token = await _tokenService.getTokenValue();
+    experienceModels = await _experienceApiService.getPublicExperiences(token: token, query: query);
+    notifyListeners();
+    return experienceModels!.results!;
+  }
+
   @override
-  Future futureToRun() async {
-    String token = await _tokenService.getTokenValue();
-    return _authService.getUserData(token: token).then((value) {
-      if (value != null) {
-        user = value;
-        notifyListeners();
-        return user;
-      } else {
-        return null;
-      }
-    });
+  Future<List<ExperienceResults?>> futureToRun() async {
+    return getPublicExperiences();
   }
 }
