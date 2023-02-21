4651299d646ef615e3c7d58d81f13ef9482e2d86
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-21 01:00:52 +0100

    commit trips view update

--- lib/ui/views/navbar/trips/trips_view.dart
+++ lib/ui/views/navbar/trips/trips_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:goasbar/ui/views/navbar/trips/trips_viewmodel.dart';
+import 'package:goasbar/shared/ui_helpers.dart';
+import 'package:goasbar/ui/widgets/loader.dart';
+import 'package:goasbar/ui/widgets/trip_card/trip_card.dart';
 import 'package:stacked/stacked.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:goasbar/ui/widgets/trips.dart';
-import 'package:goasbar/ui/widgets/load_header.dart';
+import 'package:styled_widget/styled_widget.dart';
 
 class TripsView extends HookWidget {
   const TripsView({Key? key, this.text}) : super(key: key);
@@ -20,14 +22,26 @@ class TripsView extends HookWidget {
             child: Container(
               height: double.maxFinite,
               padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: model.isBusy ? LoadHeader(text: text)
-                  : Trips(text: text, model: model),
+              child: Column(
+                children: [
+                  verticalSpaceMedium,
+                  Text(text!, style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),).alignment(Alignment.centerLeft),
+                  verticalSpaceMedium,
+                  model.isBusy ? const Loader().center() : model.experienceModels!.count == 0 ? const Text('No trips right now').center() : Expanded(child: ListView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: model.experienceModels!.count,
+                    itemBuilder: (context, index) {
+                      return TripItem(experience: model.experienceModels!.results![index]);
+                    },)
+                  ),
+                ],
+              ),
             ),
           )
         ),
       ),
       viewModelBuilder: () => TripsViewModel(),
-      onModelReady: (model) => model.loadData(),
     );
   }
 }
