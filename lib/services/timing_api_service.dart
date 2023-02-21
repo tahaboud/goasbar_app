04ff59e126b88dbfcfafa395f3953cf9a8bfbcc6
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-21 02:31:57 +0100

    commit timing api service updates

--- lib/services/timing_api_service.dart
+++ lib/services/timing_api_service.dart
@@ -25,6 +25,23 @@ class TimingApiService {
     });
   }
 
+  Future<TimingModel?> updateTiming({String? token, Map? body, int? timingId}) async {
+    return http.post(
+      Uri.parse("$baseUrl/api/experience/provider/timing/$timingId/"),
+      headers: {
+        "Accept-Language": "en-US",
+        "Authorization": "Token $token",
+      },
+      body: body,
+    ).then((response) {
+      if (response.statusCode == 200) {
+        return TimingModel.fromJson(jsonDecode(response.body));
+      } else {
+        return null;
+      }
+    });
+  }
+
   Future<TimingListModel?> getTimingsList({String? token, int? experienceId}) async {
     return http.get(
       Uri.parse("$baseUrl/api/experience/provider/timing/$experienceId/"),
