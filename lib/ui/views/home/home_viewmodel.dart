7d5be958f565e8d832a55e477c80e0e9511a25f4
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-22 02:33:49 +0100

    commit 1 update

--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -1,16 +1,27 @@
 import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/data_models/user_model.dart';
+import 'package:goasbar/services/auth_service.dart';
 import 'package:goasbar/services/token_service.dart';
 import 'package:stacked/stacked.dart';
 
 class HomeViewModel extends IndexTrackingViewModel {
   final _tokenService = locator<TokenService>();
-  bool? isTokenExist;
+  final _authService = locator<AuthService>();
+  UserModel? user;
 
-  checkToken () async {
+  Future<UserModel?> getUserData() async {
+    String token = await _tokenService.getTokenValue();
     setBusy(true);
-    isTokenExist = await _tokenService.isTokenExist();
-    setBusy(false);
-    notifyListeners();
-    return isTokenExist;
+    return _authService.getUserData(token: token).then((value) {
+      if (value != null) {
+        user = value;
+        notifyListeners();
+        setBusy(false);
+        return user;
+      } else {
+        setBusy(false);
+        return null;
+      }
+    });
   }
 }
