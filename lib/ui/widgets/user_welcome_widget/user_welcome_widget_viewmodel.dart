eef3226344ec50502679dbeaf95a4c45af88d1bf
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-22 02:34:52 +0100

    commit 1 update

--- lib/ui/widgets/user_welcome_widget/user_welcome_widget_viewmodel.dart
+++ lib/ui/widgets/user_welcome_widget/user_welcome_widget_viewmodel.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:goasbar/app/app.locator.dart';
-import 'package:goasbar/data_models/user_model.dart';
-import 'package:goasbar/services/auth_service.dart';
-import 'package:goasbar/services/token_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class UserWelcomeWidgetViewModel extends FutureViewModel<dynamic> {
+class UserWelcomeWidgetViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
-  final _tokenService = locator<TokenService>();
-  final _authService = locator<AuthService>();
-  UserModel? user;
 
   void navigateTo({view}) {
     _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
@@ -18,19 +12,5 @@ class UserWelcomeWidgetViewModel extends FutureViewModel<dynamic> {
 
   void back() {
     _navigationService.back();
-  }
-
-  @override
-  Future futureToRun() async {
-    String token = await _tokenService.getTokenValue();
-    return _authService.getUserData(token: token).then((value) {
-      if (value != null) {
-        user = value;
-        notifyListeners();
-        return user;
-      } else {
-        return null;
-      }
-    });
   }
 }
