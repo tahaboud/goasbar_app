f5cb1ef5e9d62601a334c8986a4f7f49a17e134d
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-02-22 02:34:42 +0100

    commit 1 update

--- lib/ui/views/trip_detail/trip_detail_viewmodel.dart
+++ lib/ui/views/trip_detail/trip_detail_viewmodel.dart
@@ -1,17 +1,50 @@
 import 'package:flutter/animation.dart';
 import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/data_models/experience_response.dart';
+import 'package:goasbar/data_models/public_provider_model.dart';
+import 'package:goasbar/data_models/timing_list_model.dart';
+import 'package:goasbar/data_models/user_model.dart';
+import 'package:goasbar/services/auth_service.dart';
+import 'package:goasbar/services/provider_api_service.dart';
+import 'package:goasbar/services/timing_api_service.dart';
+import 'package:goasbar/services/token_service.dart';
 import 'package:goasbar/shared/app_configs.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class TripDetailViewModel extends BaseViewModel {
+class TripDetailViewModel extends FutureViewModel<PublicProviderModel?> {
+  UserModel? user;
+  ExperienceResults? experience;
+  TripDetailViewModel({this.user, this.experience});
+
   bool isFav = false;
   int pageIndex = 1;
   final _navigationService = locator<NavigationService>();
+  final _tokenService = locator<TokenService>();
+  final _authService = locator<AuthService>();
+  final _timingApiService = locator<TimingApiService>();
+  final _providerApiService = locator<ProviderApiService>();
+  TimingListModel? timingListModel;
+  PublicProviderModel? provider;
+  List<int>? favoriteList = [];
 
-  void addFavorites() {
+  void addFavorites({int? experienceId}) {
     isFav = !isFav;
+    updateUserData(experienceId: experienceId);
     notifyListeners();
+  }
+
+  updateUserData({int? experienceId}) async {
+    String token = await _tokenService.getTokenValue();
+    notifyListeners();
+
+    favoriteList = user!.favoriteExperiences;
+
+    favoriteList!.remove(experienceId!);
+    notifyListeners();
+    _authService.updateUserData(token: token, body: {
+      "favorite_experiences": favoriteList,
+    },);
   }
 
   void changeIndex({index}) {
@@ -36,5 +69,35 @@ class TripDetailViewModel extends BaseViewModel {
     final String day = formatDay(dateTime);
     final String month = shortMonths[int.parse(dateTime.substring(5,7))];
     return '$day $month $year';
+  }
+
+  //TODO : set timings (we can have ore than one so how)
+  getTimingsList({int? experienceId}) async {
+    String? token = await _tokenService.getTokenValue();
+    timingListModel = await _timingApiService.getTimingsList(token: token, experienceId: experienceId,);
+    notifyListeners();
+  }
+
+  bool getIsFav () {
+    favoriteList = user!.favoriteExperiences;
+    isFav = favoriteList!.contains(experience!.id);
+    notifyListeners();
+
+    return isFav;
+  }
+
+  Future<PublicProviderModel?> getProvider () async {
+    String? token = await _tokenService.getTokenValue();
+    getIsFav();
+    provider = await _providerApiService.getProviderInfo(token: token, providerId: experience!.providerId);
+    notifyListeners();
+    print(provider);
+
+    return provider;
+  }
+
+  @override
+  Future<PublicProviderModel?> futureToRun() async {
+    return getProvider();
   }
 }
