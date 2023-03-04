d841c3bd16fd2e76762ef638d454eaf840d86765
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-04 13:08:53 +0100

    commit app locator update

--- lib/app/app.locator.dart
+++ lib/app/app.locator.dart
@@ -7,6 +7,7 @@
 // ignore_for_file: public_member_api_docs, implementation_imports, depend_on_referenced_packages
 
 import 'package:goasbar/services/auth_service.dart';
+import 'package:goasbar/services/booking_api_service.dart';
 import 'package:goasbar/services/experience_api_service.dart';
 import 'package:goasbar/services/provider_api_service.dart';
 import 'package:goasbar/services/timing_api_service.dart';
@@ -42,4 +43,5 @@ Future<void> setupLocator(
   locator.registerLazySingleton(() => ProviderApiService());
   locator.registerLazySingleton(() => ExperienceApiService());
   locator.registerLazySingleton(() => TimingApiService());
+  locator.registerLazySingleton(() => BookingApiService());
 }
