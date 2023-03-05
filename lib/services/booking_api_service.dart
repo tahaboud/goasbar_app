997860fe27a521638abf68a0a2859140d35dd0a8
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-05 04:06:18 +0100

    commit booking_api_service.dart update

--- lib/services/booking_api_service.dart
+++ lib/services/booking_api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:goasbar/data_models/booking_model.dart';
 import 'package:goasbar/data_models/public_provider_model.dart';
 import 'package:goasbar/shared/app_configs.dart';
 // ignore: depend_on_referenced_packages
@@ -7,17 +8,50 @@ import 'package:http/http.dart' as http;
 import 'package:goasbar/data_models/provider_model.dart';
 
 class BookingApiService {
-  Future<ProviderModel?> createProvider({String? token, Map? body}) async {
+  Future<BookingModel?> createBooking({String? token, int? timingId}) async {
     return http.post(
-      Uri.parse("$baseUrl/api/provider/"),
+      Uri.parse("$baseUrl/api/booking/$timingId/"),
+      headers: {
+        "Accept-Language": "en-US",
+        "Authorization": "Token $token",
+      },
+    ).then((response) {
+      if (response.statusCode == 201) {
+        return BookingModel.fromJson(jsonDecode(response.body));
+      } else {
+        return null;
+      }
+    });
+  }
+
+  Future<String?> prepareCheckout({String? token, int? bookingId, Map? body}) async {
+    return http.post(
+      Uri.parse("$baseUrl/api/booking/payment/$bookingId/"),
       headers: {
         "Accept-Language": "en-US",
         "Authorization": "Token $token",
       },
       body: body,
     ).then((response) {
-      if (response.statusCode == 201) {
-        return ProviderModel.fromJson(jsonDecode(response.body));
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body)['checkoutId'];
+      } else {
+        return null;
+      }
+    });
+  }
+
+  Future<String?> getPaymentStatus({String? token, int? bookingId}) async {
+    return http.get(
+      Uri.parse("$baseUrl/api/booking/payment/$bookingId/"),
+      headers: {
+        "Accept-Language": "en-US",
+        "Authorization": "Token $token",
+      },
+    ).then((response) {
+      print(response.body);
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body)['response'];
       } else {
         return null;
       }
