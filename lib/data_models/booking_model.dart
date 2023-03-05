c5bdf11d1b42717d545181dce231da361b7d549d
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-05 04:02:06 +0100

    push booking model

---
+++ lib/data_models/booking_model.dart
@@ -0,0 +1,21 @@
+import 'package:goasbar/data_models/booking_response.dart';
+
+class BookingModel {
+  BookingResponse? response;
+
+  BookingModel({this.response});
+
+  BookingModel.fromJson(Map<String, dynamic> json) {
+    response = json['response'] != null
+        ? BookingResponse.fromJson(json['response'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (response != null) {
+      data['response'] = response!.toJson();
+    }
+    return data;
+  }
+}
