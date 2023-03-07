485e70ddb0f00b8e484024c4b27064fd63a8eebb
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-07 23:29:02 +0100

    push review api service

---
+++ lib/services/review_api_service.dart
@@ -0,0 +1,78 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'dart:convert';
+
+import 'package:goasbar/data_models/timing_list_model.dart';
+import 'package:goasbar/data_models/timing_model.dart';
+import 'package:goasbar/shared/app_configs.dart';
+import 'package:http/http.dart' as http;
+
+class ReviewApiService {
+  //TODO add review model
+  Future<TimingModel?> createReview({String? token, Map? body, int? bookingId}) async {
+    return http.post(
+      Uri.parse("$baseUrl/api/review/$bookingId/"),
+      headers: {
+        "Accept-Language": "en-US",
+        "Authorization": "Token $token",
+      },
+      body: body,
+    ).then((response) {
+      if (response.statusCode == 201) {
+        return TimingModel.fromJson(jsonDecode(response.body));
+      } else {
+        return null;
+      }
+    });
+  }
+
+  Future<TimingModel?> updateTiming({String? token, Map? body, int? timingId}) async {
+    return http.post(
+      Uri.parse("$baseUrl/api/experience/provider/timing/$timingId/"),
+      headers: {
+        "Accept-Language": "en-US",
+        "Authorization": "Token $token",
+      },
+      body: body,
+    ).then((response) {
+      print(response.body);
+      if (response.statusCode == 200) {
+        return TimingModel.fromJson(jsonDecode(response.body));
+      } else {
+        return null;
+      }
+    });
+  }
+
+  Future<TimingListModel?> getTimingsList({String? token, int? experienceId}) async {
+    return http.get(
+      Uri.parse("$baseUrl/api/experience/provider/timing/$experienceId/"),
+      headers: {
+        "Accept-Language": "en-US",
+        "Authorization": "Token $token",
+      },
+    ).then((response) {
+      if (response.statusCode == 200) {
+        return TimingListModel.fromJson(jsonDecode(response.body));
+      } else {
+        return null;
+      }
+    });
+  }
+
+  Future<bool?> deleteTiming({String? token, int? timingId}) async {
+    return http.delete(
+      Uri.parse("$baseUrl/api/experience/provider/timing/$timingId/"),
+      headers: {
+        "Accept-Language": "en-US",
+        "Authorization": "Token $token",
+      },
+    ).then((response) {
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  }
+}
