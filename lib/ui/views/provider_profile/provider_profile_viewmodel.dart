0daf4f448736226a8d0e367fd82473f8ad7e5f6b
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-07 23:28:28 +0100

    push provider public viewmodel

---
+++ lib/ui/views/provider_profile/provider_profile_viewmodel.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:goasbar/app/app.locator.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class ProviderProfileViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+
+  void navigateTo({view}) {
+    _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
+  }
+
+  void clearAndNavigateTo({view}) {
+    _navigationService.clearStackAndShowView(view,);
+  }
+
+  void back() {
+    _navigationService.back();
+  }
+}
