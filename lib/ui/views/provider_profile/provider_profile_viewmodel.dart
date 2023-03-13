9efd3561212e382937f1d40be01845f88e3bb805
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-13 23:54:36 +0100

    commit provider profile view model

--- lib/ui/views/provider_profile/provider_profile_viewmodel.dart
+++ lib/ui/views/provider_profile/provider_profile_viewmodel.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/data_models/provider_public_experience_model.dart';
+import 'package:goasbar/data_models/provider_public_experience_response.dart';
+import 'package:goasbar/services/provider_api_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class ProviderProfileViewModel extends BaseViewModel {
+class ProviderProfileViewModel extends FutureViewModel<List<ProviderPublicExperienceResults?>?> {
+  int? providerId;
+  ProviderProfileViewModel({this.providerId});
+
   final _navigationService = locator<NavigationService>();
+  final _providerApiService = locator<ProviderApiService>();
+  ProviderPublicExperienceModel? providerPublicExperience;
 
   void navigateTo({view}) {
     _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
@@ -16,5 +24,16 @@ class ProviderProfileViewModel extends BaseViewModel {
 
   void back() {
     _navigationService.back();
+  }
+
+  Future<List<ProviderPublicExperienceResults>?>? getProviderPublicExperiences() async {
+    providerPublicExperience = await _providerApiService.getProviderPublicExperiences(providerId: providerId);
+    notifyListeners();
+    return providerPublicExperience!.results!;
+  }
+
+  @override
+  Future<List<ProviderPublicExperienceResults?>?> futureToRun() async {
+    return getProviderPublicExperiences();
   }
 }
