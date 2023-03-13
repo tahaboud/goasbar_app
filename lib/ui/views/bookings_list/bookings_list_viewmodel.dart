f543afe0400d14b2f667d1e606641a5f5e3b05e7
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-14 00:19:19 +0100

    push booking list view model

---
+++ lib/ui/views/bookings_list/bookings_list_viewmodel.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/data_models/bookings_list_model.dart';
+import 'package:goasbar/data_models/bookings_list_response.dart';
+import 'package:goasbar/services/booking_api_service.dart';
+import 'package:goasbar/services/token_service.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class BookingsListViewModel extends FutureViewModel<List<BookingsListResults?>?> {
+  BuildContext? context;
+  BookingsListViewModel({this.context});
+
+  int index = 1;
+  final _navigationService = locator<NavigationService>();
+  final _bookingApiService = locator<BookingApiService>();
+  final _tokenService = locator<TokenService>();
+  bool? isTokenExist;
+  BookingsListModel? bookingsListModel;
+
+  void navigateTo({view}) {
+    _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
+  }
+
+  void back() {
+    _navigationService.back();
+  }
+
+  Future<bool?> deleteBooking({int? bookingId, context}) async {
+    String? token = await _tokenService.getTokenValue();
+    setBusy(true);
+    return await _bookingApiService.deleteBooking(context: context, token: token, bookingId: bookingId).then((value) async {
+      data = await futureToRun();
+      setBusy(false);
+      return value;
+    });
+  }
+
+  Future<List<BookingsListResults?>?> getUserBookings() async {
+    String? token = await _tokenService.getTokenValue();
+    bookingsListModel = await _bookingApiService.getUserBookings(token: token, context: context);
+    notifyListeners();
+    return bookingsListModel!.results!;
+  }
+
+  @override
+  Future<List<BookingsListResults?>?> futureToRun() async {
+    return await getUserBookings();
+  }
+}
