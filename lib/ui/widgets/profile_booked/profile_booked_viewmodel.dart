712004f02c52373fb295c2b8edc583e0669df154
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-14 00:19:40 +0100

    push profile booked vm

---
+++ lib/ui/widgets/profile_booked/profile_booked_viewmodel.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/enum/dialog_type.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class ProfileBookedViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+  final _dialogService = locator<DialogService>();
+
+  void navigateTo({view}) {
+    _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
+  }
+
+  void back() {
+    _navigationService.back();
+  }
+
+  showProfile({user, experienceId}) {
+    _dialogService.showCustomDialog(
+      variant: DialogType.profile,
+      data: user,
+      customData: experienceId,
+    );
+  }
+}
