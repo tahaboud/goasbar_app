764504a17b975c70286d67668299f1934cc42001
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-13 23:55:25 +0100

    commit review vm

--- lib/ui/views/review/review_viewmodel.dart
+++ lib/ui/views/review/review_viewmodel.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/data_models/review_model.dart';
+import 'package:goasbar/services/review_api_service.dart';
+import 'package:goasbar/services/token_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class ReviewViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
+  final _tokenService = locator<TokenService>();
+  final _reviewApiService = locator<ReviewApiService>();
   double? rating = 0;
+  bool? isClicked = false;
+
+  updateIsClicked({value}) {
+    isClicked = value;
+    notifyListeners();
+  }
 
   void navigateTo({view}) {
     _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
@@ -22,5 +33,11 @@ class ReviewViewModel extends BaseViewModel {
   updateRating({double? value}) {
     rating = value;
     notifyListeners();
+  }
+
+  Future<ReviewModel?> createReview({context, Map? body, int? bookingId}) async {
+    updateIsClicked(value: true);
+    String? token = await _tokenService.getTokenValue();
+    return await _reviewApiService.createReview(token: token, body: body, bookingId: bookingId, context: context!);
   }
 }
