2ebc7a540572e53a97bd225679e6e311f0f20029
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-14 00:19:52 +0100

    push profile timing booking

---
+++ lib/data_models/provider_timing_booking.dart
@@ -0,0 +1,34 @@
+import 'package:goasbar/data_models/provider_timing_booking_response.dart';
+
+class ProviderTimingBooking {
+  int? count;
+  dynamic next;
+  dynamic previous;
+  List<ProviderTimingBookingResults>? results;
+
+  ProviderTimingBooking({this.count, this.next, this.previous, this.results});
+
+  ProviderTimingBooking.fromJson(Map<String, dynamic> json) {
+    count = json['count'];
+    next = json['next'];
+    previous = json['previous'];
+    if (json['results'] != null) {
+      results = <ProviderTimingBookingResults>[];
+      json['results'].forEach((v) {
+        results!.add(ProviderTimingBookingResults.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['count'] = count;
+    data['next'] = next;
+    data['previous'] = previous;
+    if (results != null) {
+      data['results'] = results!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
