6489e657fcf5436f518dd036d267666818fa70bd
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-22 01:50:36 +0100

    commit home vm

--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -1,15 +1,17 @@
 import 'dart:async';
-
 import 'package:goasbar/app/app.locator.dart';
 import 'package:goasbar/data_models/user_model.dart';
 import 'package:goasbar/services/auth_service.dart';
+import 'package:goasbar/services/connectivity_service.dart';
 import 'package:goasbar/services/token_service.dart';
 import 'package:stacked/stacked.dart';
 
 class HomeViewModel extends IndexTrackingViewModel {
   final _tokenService = locator<TokenService>();
   final _authService = locator<AuthService>();
+  final _connectivityService = locator<ConnectivityService>();
   UserModel? user;
+  bool? thereIsConnection = true;
 
   Future<UserModel?> getUserData({context}) async {
     String token = await _tokenService.getTokenValue();
@@ -24,6 +26,17 @@ class HomeViewModel extends IndexTrackingViewModel {
         setBusy(false);
         return null;
       }
+    });
+  }
+
+  getConnectivityStatus({context}) {
+    _connectivityService.checkConnectivity(() {
+      getUserData(context: context);
+      thereIsConnection = true;
+      notifyListeners();
+    }, () {
+      thereIsConnection = false;
+      notifyListeners();
     });
   }
 }
