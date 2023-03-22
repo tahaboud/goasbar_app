56ebe3c5c7072402503e8b0dfe71767ce3efe247
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-22 01:51:22 +0100

    commit messages_view.dart

--- lib/ui/views/messages/messages_view.dart
+++ lib/ui/views/messages/messages_view.dart
@@ -1,24 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:goasbar/data_models/user_model.dart';
 import 'package:goasbar/shared/colors.dart';
 import 'package:goasbar/shared/ui_helpers.dart';
 import 'package:goasbar/ui/views/chat_with_agency/chat_with_agency_view.dart';
 import 'package:goasbar/ui/views/messages/messages_viewmodel.dart';
-import 'package:goasbar/ui/widgets/chat_item.dart';
+import 'package:goasbar/ui/widgets/chat_item/chat_item.dart';
 import 'package:styled_widget/styled_widget.dart';
 import 'package:goasbar/ui/widgets/loader.dart';
 import 'package:stacked/stacked.dart';
 
 class MessagesView extends HookWidget {
-  const MessagesView({
-    Key? key,
-  }) : super(key: key);
+  const MessagesView({Key? key, this.user,}) : super(key: key,);
+  final UserModel? user;
 
   @override
   Widget build(BuildContext context) {
     var search = useTextEditingController();
 
-    return ViewModelBuilder.reactive(
+    return ViewModelBuilder<MessagesViewModel>.reactive(
       builder: (context, model, child) {
         return model.isBusy ? const Loader() : Column(
           children: [
@@ -42,17 +43,46 @@ class MessagesView extends HookWidget {
             verticalSpaceMedium,
             const Text('Chats', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),).alignment(Alignment.centerLeft),
             verticalSpaceMedium,
-            ListView(
-              shrinkWrap: true,
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              children: [
-                ChatItem(onTap: () => model.navigateTo(view: const ChatWithAgencyView())),
-                ChatItem(onTap: () => model.navigateTo(view: const ChatWithAgencyView())),
-                ChatItem(onTap: () => model.navigateTo(view: const ChatWithAgencyView())),
-                ChatItem(onTap: () => model.navigateTo(view: const ChatWithAgencyView())),
-                ChatItem(onTap: () => model.navigateTo(view: const ChatWithAgencyView())),
-                ChatItem(onTap: () => model.navigateTo(view: const ChatWithAgencyView())),
-              ],
+            model.isBusy ? const Loader().center() : StreamBuilder(
+              stream: model.stream,
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.transparent, borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  );
+                }
+
+                List<Widget>? messagesItems = [];
+                List<DocumentSnapshot>? chats = snapshot.data!.docs;
+
+                for (var chat in chats) {
+                  Map<String, dynamic> data = chat.data()! as Map<String, dynamic>;
+
+                  messagesItems.add(
+                    ChatItem(
+                      isUser: !user!.isProvider!,
+                      receiverName: user!.id == data["members"][1] ? data["member_names"][0] : data["member_names"][1],
+                      receiverId: user!.id == data["members"][1] ? int.parse(data["members"][0]) : int.parse(data["members"][1]),
+                      onTap: () => model.navigateTo(view: const ChatWithAgencyView()),
+                    ),
+                  );
+                }
+
+                return SingleChildScrollView(
+                  reverse: true,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: messagesItems,
+                  ),
+                );
+              },
             ),
           ],
         );
