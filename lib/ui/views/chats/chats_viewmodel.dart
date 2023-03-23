22d933c531729c49845fd7e301ba30d4d7addf65
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-23 15:28:29 +0100

    commit chats view model

--- lib/ui/views/chats/chats_viewmodel.dart
+++ lib/ui/views/chats/chats_viewmodel.dart
@@ -5,22 +5,13 @@ import 'package:goasbar/app/app.locator.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class MessagesViewModel extends StreamViewModel<QuerySnapshot<Map<String, dynamic>>> {
+class ChatsViewModel extends StreamViewModel<QuerySnapshot<Map<String, dynamic>>> {
   bool isDone = false;
   final _navigationService = locator<NavigationService>();
   final _fireStore = FirebaseFirestore.instance;
 
-  startAnimation() async {
-    Timer(const Duration(milliseconds: 1000), () async { isDone = true; notifyListeners(); },);
-  }
-
   void navigateTo({view}) {
     _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
-  }
-
-  loadData() async {
-    setBusy(true);
-    Timer(const Duration(milliseconds: 1000), () { setBusy(false); },);
   }
 
   Stream<QuerySnapshot<Map<String, dynamic>>> getSnapshots() {
