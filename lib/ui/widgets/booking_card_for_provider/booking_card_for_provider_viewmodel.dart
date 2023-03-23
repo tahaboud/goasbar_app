35ebeb372d8c5bd68b4f7c738e3967b0090d4d95
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-23 15:08:07 +0100

    commit booking card for provider vm

--- lib/ui/widgets/booking_card_for_provider/booking_card_for_provider_viewmodel.dart
+++ lib/ui/widgets/booking_card_for_provider/booking_card_for_provider_viewmodel.dart
@@ -1,32 +1,33 @@
 import 'package:flutter/animation.dart';
 import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/data_models/provider_public_experience_response.dart';
 import 'package:goasbar/data_models/user_model.dart';
-import 'package:goasbar/enum/bottom_sheet_type.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class BookingItemForProviderViewModel extends BaseViewModel {
+class BookingItemForProviderViewModel extends FutureViewModel<bool?> {
   final UserModel? user;
-  BookingItemForProviderViewModel({this.user, });
+  final ProviderPublicExperienceResults? experience;
+  BookingItemForProviderViewModel({this.user, this.experience});
 
   final _navigationService = locator<NavigationService>();
-  final _bottomSheetService = locator<BottomSheetService>();
   List<int>? favoriteList = [];
+  bool isFav = false;
 
   navigateTo({view}) async {
     await _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
   }
 
-  Future showReviewBottomSheet() async {
-    var response = await _bottomSheetService.showCustomSheet(
-      variant: BottomSheetType.review,
-      isScrollControlled: true,
-      barrierDismissible: true,
-      data: user,
-    );
+  bool getIsFav () {
+    favoriteList = user!.favoriteExperiences;
+    isFav = favoriteList!.contains(experience!.id);
+    notifyListeners();
 
-    if (response!.confirmed) {
-      return true;
-    }
+    return isFav;
+  }
+
+  @override
+  Future<bool?> futureToRun() async {
+    return getIsFav();
   }
 }
