a8fc2e2633bc2d1f5174d0f0487119caccc12760
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-25 09:56:56 +0100

    commit trips vm updates

--- lib/ui/views/navbar/trips/trips_viewmodel.dart
+++ lib/ui/views/navbar/trips/trips_viewmodel.dart
@@ -1,36 +1,28 @@
 import 'dart:async';
 
-import 'package:flutter/animation.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:goasbar/app/app.locator.dart';
 import 'package:goasbar/data_models/experience_model.dart';
 import 'package:goasbar/data_models/experience_response.dart';
 import 'package:goasbar/services/experience_api_service.dart';
 import 'package:stacked/stacked.dart';
-import 'package:stacked_services/stacked_services.dart';
 
 class TripsViewModel extends FutureViewModel<List<ExperienceResults?>> {
-  int index = 1;
-  final _navigationService = locator<NavigationService>();
   final _experienceApiService = locator<ExperienceApiService>();
-  bool? isTokenExist;
   ExperienceModel? experienceModels;
+  int pageNumber = 1;
 
-  void selectCategory({ind}) {
-    index = ind;
-    notifyListeners();
-  }
-
-  void navigateTo({view}) {
-    _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
-  }
-
-  void back() {
-    _navigationService.back();
+  Future getPublicExperiencesFromNextPage({int? index}) async {
+    if (index! % 10 == 0) {
+      pageNumber++;
+      print("index : $index");
+      ExperienceModel? experienceModelsList = await _experienceApiService.getPublicExperiences(page: pageNumber);
+      experienceModels!.results!.addAll(experienceModelsList!.results!);
+      notifyListeners();
+    }
   }
 
   Future<List<ExperienceResults?>> getPublicExperiences({String? query,}) async {
-    experienceModels = await _experienceApiService.getPublicExperiences(query: query,);
+    experienceModels = await _experienceApiService.getPublicExperiences(query: query, page: pageNumber);
     notifyListeners();
     return experienceModels!.results!;
   }
