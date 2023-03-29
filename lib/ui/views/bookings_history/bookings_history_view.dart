6b1caf77e4c30210b4301e49b946637802f17c98
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-29 18:43:28 +0100

    commit bookings_history_view.dart

--- lib/ui/views/bookings_history/bookings_history_view.dart
+++ lib/ui/views/bookings_history/bookings_history_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:goasbar/data_models/user_model.dart';
 import 'package:goasbar/shared/ui_helpers.dart';
-import 'package:goasbar/ui/views/bookings_list/bookings_list_viewmodel.dart';
+import 'package:goasbar/ui/views/bookings_history/bookings_history_viewmodel.dart';
 import 'package:goasbar/ui/widgets/booking_card/booking_card.dart';
 import 'package:goasbar/ui/widgets/creation_aware_item.dart';
 import 'package:goasbar/ui/widgets/loader.dart';
@@ -11,13 +11,13 @@ import 'package:stacked/stacked.dart';
 import 'package:styled_widget/styled_widget.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-class BookingsListView extends HookWidget {
-  const BookingsListView({Key? key, this.user}) : super(key: key);
+class BookingsHistoryView extends HookWidget {
+  const BookingsHistoryView({Key? key, this.user}) : super(key: key);
   final UserModel? user;
 
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<BookingsListViewModel>.reactive(
+    return ViewModelBuilder<BookingsHistoryViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
         body: SafeArea(
           child: Container(
@@ -35,7 +35,7 @@ class BookingsListView extends HookWidget {
                         model.back();
                       },
                     ).alignment(Alignment.centerLeft),
-                    const Text('Bookings List', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                    const Text('Bookings History', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
                   ],
                 ),
                 verticalSpaceMedium,
@@ -48,17 +48,22 @@ class BookingsListView extends HookWidget {
                       itemBuilder: (ctx, index) {
                         return CreationAwareListItem(
                           itemCreated: () => model.getUserBookingsFromNextPage(index: index + 1),
-                          child: BookingItem(bookingsList: model.data![index], user: user, onDelete: () {
-                            model.deleteBooking(context: context, bookingId: model.data![index]!.id).then((value) {
-                              if (value != null) {
-                                if (value) {
-                                  showMotionToast(context: context, title: 'Deleted Booking Successfully', msg: "The booking is deleted, you will receive your refund", type: MotionToastType.success);
-                                } else {
-                                  showMotionToast(context: context, title: 'Deleted Booking Failed', msg: "This booking cannot be canceled", type: MotionToastType.error);
-                                }
-                              }
-                            });
-                          }),
+                          child: BookingItem(
+                            bookingsList: model.data![index], user: user,
+                            onUpdate: () => model.futureToRun(),
+                            onDelete: () {
+                                model.deleteBooking(context: context, bookingId: model.data![index]!.id).then((value) {
+                                  if (value != null) {
+                                    if (value) {
+                                      showMotionToast(context: context, title: 'Deleted Booking Successfully', msg: "The booking is deleted, you will receive your refund", type: MotionToastType.success);
+                                    } else {
+                                      showMotionToast(context: context, title: 'Deleted Booking Failed', msg: "This booking cannot be canceled", type: MotionToastType.error);
+                                    }
+                                  }
+                                },
+                              );
+                            },
+                          ),
                         );
                       },
                     )
@@ -68,7 +73,7 @@ class BookingsListView extends HookWidget {
           )
         ),
       ),
-      viewModelBuilder: () => BookingsListViewModel(context: context),
+      viewModelBuilder: () => BookingsHistoryViewModel(context: context),
     );
   }
 }
