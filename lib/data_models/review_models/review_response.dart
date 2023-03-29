bfc950a8e4a0bfd4f03183bac77536e43b61a335
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-03-29 18:44:40 +0100

    commit review response

--- lib/data_models/review_models/review_response.dart
+++ lib/data_models/review_models/review_response.dart
@@ -1,15 +1,19 @@
+import 'package:goasbar/data_models/review_models/user_for_review.dart';
+
 class ReviewResponse {
   int? id;
   int? booking;
+  UserForReview? user;
   String? rate;
-  dynamic comment;
+  String? comment;
   String? creationDate;
 
-  ReviewResponse({this.id, this.booking, this.rate, this.comment, this.creationDate});
+  ReviewResponse({this.id, this.user, this.rate, this.comment, this.creationDate});
 
   ReviewResponse.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     booking = json['booking'];
+    user = json['user'] != null ? UserForReview.fromJson(json['user']) : null;
     rate = json['rate'];
     comment = json['comment'];
     creationDate = json['creation_date'];
@@ -19,6 +23,9 @@ class ReviewResponse {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['id'] = id;
     data['booking'] = booking;
+    if (user != null) {
+      data['user'] = user!.toJson();
+    }
     data['rate'] = rate;
     data['comment'] = comment;
     data['creation_date'] = creationDate;
