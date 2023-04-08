b0424f1723ff9322249f9db5dd7cbc6661f3ff9d
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-04-08 17:08:23 +0100

    commit provider profile view model

--- lib/ui/views/provider_profile/provider_profile_viewmodel.dart
+++ lib/ui/views/provider_profile/provider_profile_viewmodel.dart
@@ -3,6 +3,7 @@ import 'package:goasbar/app/app.locator.dart';
 import 'package:goasbar/data_models/provider_public_experience_model.dart';
 import 'package:goasbar/data_models/provider_public_experience_response.dart';
 import 'package:goasbar/services/provider_api_service.dart';
+import 'package:goasbar/services/url_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -12,6 +13,7 @@ class ProviderProfileViewModel extends FutureViewModel<List<ProviderPublicExperi
 
   final _navigationService = locator<NavigationService>();
   final _providerApiService = locator<ProviderApiService>();
+  final _urlService = locator<UrlService>();
   ProviderPublicExperienceModel? providerPublicExperience;
   int pageNumber = 1;
 
@@ -25,6 +27,18 @@ class ProviderProfileViewModel extends FutureViewModel<List<ProviderPublicExperi
 
   void back() {
     _navigationService.back();
+  }
+
+  void launchEmail({String? email}) {
+    _urlService.launchEmail(email: email);
+  }
+
+  void launchPhoneCall({String? phoneNumber}) {
+    _urlService.launchPhoneCall(phoneNumber: phoneNumber);
+  }
+
+  void launchLink({String? link}) {
+    _urlService.launchLink(link: link!);
   }
 
   Future getProviderPublicExperiencesFromNextPage({int? index}) async {
