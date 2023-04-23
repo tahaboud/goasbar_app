c15329c7c527c8a000cc887d3a8a7344951bf47e
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-04-23 17:10:18 +0100

    commit trips_view.dart

--- lib/ui/views/navbar/trips/trips_view.dart
+++ lib/ui/views/navbar/trips/trips_view.dart
@@ -3,9 +3,10 @@ import 'package:goasbar/data_models/user_model.dart';
 import 'package:goasbar/shared/colors.dart';
 import 'package:goasbar/ui/views/navbar/trips/trips_viewmodel.dart';
 import 'package:goasbar/shared/ui_helpers.dart';
+import 'package:goasbar/ui/widgets/booking_card/booking_card.dart';
 import 'package:goasbar/ui/widgets/creation_aware_item.dart';
 import 'package:goasbar/ui/widgets/loader.dart';
-import 'package:goasbar/ui/widgets/trip_card/trip_card.dart';
+import 'package:motion_toast/resources/arrays.dart';
 import 'package:stacked/stacked.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:styled_widget/styled_widget.dart';
@@ -47,16 +48,32 @@ class TripsView extends HookWidget {
                   ),
                   verticalSpaceMedium,
                   model.isBusy ? const Loader().center()
-                      : !model.dataReady ? const Text('No trips right now').center() : model.experienceModels!.count == 0
-                      ? const Text('No trips right now').center() : model.data!.isEmpty
+                      : !model.dataReady ? const Text('No trips right now').center() : model.data!.isEmpty
                       ? const Text('No trips right now').center() : Expanded(child: ListView.builder(
                     physics: const BouncingScrollPhysics(),
                     shrinkWrap: true,
-                    itemCount: model.experienceModels!.count,
-                    itemBuilder: (context, index) {
+                    itemCount: model.data!.length,
+                    itemBuilder: (ctx, index) {
                       return CreationAwareListItem(
-                        itemCreated: () => model.getPublicExperiencesFromNextPage(index: index + 1),
-                        child: TripItem(experience: model.experienceModels!.results![index], user: user,),
+                        itemCreated: () => model.getUserBookingsFromNextPage(index: index + 1),
+                        child: BookingItem(
+                          bookingsList: model.data![index],
+                          onUpdate: () {
+                            model.showReviewBottomSheet(user: user, review: model.data![index]!.review, bookingId: model.data![index]!.id);
+                          },
+                          onDelete: () {
+                            model.deleteBooking(context: context, bookingId: model.data![index]!.id).then((value) {
+                              if (value != null) {
+                                if (value) {
+                                  showMotionToast(context: context, title: 'Deleted Booking Successfully', msg: "Booking deleted, you will receive your refund", type: MotionToastType.success);
+                                } else {
+                                  showMotionToast(context: context, title: 'Deleted Booking Failed', msg: "This booking cannot be canceled", type: MotionToastType.error);
+                                }
+                              }
+                            },
+                            );
+                          },
+                        ),
                       );
                     },)
                   ),
@@ -75,7 +92,7 @@ class TripsView extends HookWidget {
       padding: const EdgeInsets.symmetric(vertical: 12,horizontal: 10),
       child: Row(
         children: [
-          image != null ? Image.asset("assets/icons/settings/$image.png", color: index == 0 ? Colors.white : Colors.black,)
+          image != null ? Image.asset("assets/icons/settings/$image.png", color: index == model!.index ? Colors.white : Colors.grey,)
               : Container(),
           image != null ? horizontalSpaceTiny : Container(),
           Text(text!, style: TextStyle(color: model!.index == index ? Colors.white : kMainGray,)),
