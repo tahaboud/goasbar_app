e0eb7abdba5ad8117bc5efa16cb805b7891dc56e
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-04-23 17:07:03 +0100

    commit add_payment_method_viewmodel.dart

--- lib/ui/views/add_payment_method/add_payment_method_viewmodel.dart
+++ lib/ui/views/add_payment_method/add_payment_method_viewmodel.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/animation.dart';
+import 'package:flutter/services.dart';
 import 'package:goasbar/app/app.locator.dart';
+import 'package:goasbar/services/auth_service.dart';
+import 'package:goasbar/services/token_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class AddPaymentMethodViewModel extends BaseViewModel {
-  int? isSelected = 1;
+  static const platform = MethodChannel('Hyperpay.demo.fultter/channel');
+  int? selectedCardType = 1;
   final _navigationService = locator<NavigationService>();
+  final _tokenService = locator<TokenService>();
+  final _authService = locator<AuthService>();
 
   void navigateTo({view}) {
     _navigationService.navigateWithTransition(view, curve: Curves.easeIn, duration: const Duration(milliseconds: 300));
@@ -16,7 +22,33 @@ class AddPaymentMethodViewModel extends BaseViewModel {
   }
 
   changeSelection({index}) {
-    isSelected = index;
+    selectedCardType = index;
     notifyListeners();
+  }
+
+  Future<String?> saveCard({String? cardType, cardNumber, cardHolder, cVV, expiryMonth, expiryYear, context}) async {
+    String? token = await _tokenService.getTokenValue();
+    await _authService.getRegistrationId(body: {"card_type": cardType,}, context: context, token: token).then((value) async {
+      String transactionStatus;
+
+      try {
+        final String result = await platform.invokeMethod('savecard', {
+          "checkoutid": value,
+          "number": cardNumber,
+          "brand": cardType,
+          "holder": cardHolder,
+          "expiryMonth": expiryMonth,
+          "expiryYear": "20$expiryYear",
+          "cvv": cVV,
+        });
+        transactionStatus = '$result';
+      } on PlatformException catch (e) {
+        transactionStatus = "${e.message}";
+      }
+
+      print(transactionStatus);
+      print("dddddddddddddddddddddddddddddddddddddddddd");
+    });
+
   }
 }
