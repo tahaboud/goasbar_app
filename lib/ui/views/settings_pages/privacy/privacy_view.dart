e653012e5ecac2b572da217c58c33405ca87701a
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-05-01 09:15:19 +0100

    commit privacy_view.dart

--- lib/ui/views/settings_pages/privacy/privacy_view.dart
+++ lib/ui/views/settings_pages/privacy/privacy_view.dart
@@ -32,7 +32,7 @@ class PrivacyView extends HookWidget {
                   ],
                 ),
                 verticalSpaceMedium,
-                const Text("1. Types data we collect", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),).alignment(Alignment.centerLeft),
+                const Text("1. Types of data that can be collected", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),).alignment(Alignment.centerLeft),
                 verticalSpaceRegular,
                 const Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident."),
                 verticalSpaceMedium,
