2542d03680c7340034030a623105536c7bd34889
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-05-01 09:17:01 +0100

    commit add_payment_method_card.dart

--- lib/ui/views/add_payment_method/add_payment_method_viewmodel.dart
+++ lib/ui/views/add_payment_method/add_payment_method_viewmodel.dart
@@ -3,6 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:goasbar/app/app.locator.dart';
 import 'package:goasbar/services/auth_service.dart';
 import 'package:goasbar/services/token_service.dart';
+import 'package:goasbar/shared/ui_helpers.dart';
+import 'package:motion_toast/resources/arrays.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -29,25 +31,11 @@ class AddPaymentMethodViewModel extends BaseViewModel {
   Future<String?> saveCard({String? cardType, cardNumber, cardHolder, cVV, expiryMonth, expiryYear, context}) async {
     String? token = await _tokenService.getTokenValue();
     await _authService.getRegistrationId(body: {"card_type": cardType,}, context: context, token: token).then((value) async {
-      String transactionStatus;
-
-      try {
-        final String result = await platform.invokeMethod('savecard', {
-          "checkoutid": value,
-          "number": cardNumber,
-          "brand": cardType,
-          "holder": cardHolder,
-          "expiryMonth": expiryMonth,
-          "expiryYear": "20$expiryYear",
-          "cvv": cVV,
-        });
-        transactionStatus = '$result';
-      } on PlatformException catch (e) {
-        transactionStatus = "${e.message}";
+      if (value != null) {
+        print('Done');
+      } else {
+        showMotionToast(context: context, title: 'Error', msg: "Failed to save card", type: MotionToastType.error);
       }
-
-      print(transactionStatus);
-      print("dddddddddddddddddddddddddddddddddddddddddd");
     });
 
   }
