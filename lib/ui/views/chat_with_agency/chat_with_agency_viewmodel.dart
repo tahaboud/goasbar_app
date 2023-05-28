d0f543b1cb2c808e6ad76ffc1584f6b30572f129
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-05-28 11:48:05 +0100

    commit chat_with_agency_viewmodel.dart.dart

--- lib/ui/views/chat_with_agency/chat_with_agency_viewmodel.dart
+++ lib/ui/views/chat_with_agency/chat_with_agency_viewmodel.dart
@@ -1,22 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:goasbar/app/app.locator.dart';
-import 'package:goasbar/data_models/chat_token_provider_model.dart';
-import 'package:goasbar/services/chat_api_service.dart';
-import 'package:goasbar/services/token_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
-class ChatWithAgencyViewModel extends FutureViewModel<ChatTokenProviderModel?> {
-  ChatWithAgencyViewModel({this.context, this.providerId});
+class ChatWithAgencyViewModel extends BaseViewModel {
+  ChatWithAgencyViewModel({this.context, });
   BuildContext? context;
-  int? providerId;
 
   final _navigationService = locator<NavigationService>();
-  final _chatApiService = locator<ChatApiService>();
-  final _tokenService = locator<TokenService>();
-  ChatTokenProviderModel? chatTokenProvider;
   int indexTab = 1;
   final _fireStore = FirebaseFirestore.instance;
 
@@ -33,61 +25,17 @@ class ChatWithAgencyViewModel extends FutureViewModel<ChatTokenProviderModel?> {
     notifyListeners();
   }
 
-  Future<ChatTokenProviderModel?> getUserFireStoreTokenAndProviderChatId() async {
-    String? token = await _tokenService.getTokenValue();
-    chatTokenProvider = await _chatApiService.getUserFireStoreTokenAndProviderChatId(context: context, token: token, providerId: providerId);
-    notifyListeners();
-    return await auth(token: chatTokenProvider!.fireStoreToken).then((value) {
-      if (value != null) {
-        return chatTokenProvider;
-      } else {
-        return null;
-      }
-    });
-  }
-
-  Future<UserCredential?> auth({token}) async {
-    try {
-      final userCredential = await FirebaseAuth.instance.signInWithCustomToken(token);
-      print("Sign-in successful.");
-      return userCredential;
-    } on FirebaseAuthException catch (e) {
-      switch (e.code) {
-        case "invalid-custom-token":
-          print("The supplied token is not a Firebase custom auth token.");
-          return null;
-        case "custom-token-mismatch":
-          print("The supplied token is for a different Firebase project.");
-          return null;
-        default:
-          print("Unkown error.");
-          return null;
-      }
-    }
-  }
-
-  file() {
-
-  }
-
-  Future sendMessage({String? userId, String? providerId, TextEditingController? message,}) async {
+  Future sendMessage({String? meId, String? notMeId, TextEditingController? message, chatId}) async {
     if (message!.text.isNotEmpty) {
-      print(chatTokenProvider!.chatId);
-
-      await _fireStore.collection("chats").doc(chatTokenProvider!.chatId).collection("messages").doc().set({
+      await _fireStore.collection("chats").doc(chatId).collection("messages").doc().set({
         "createdAt": DateTime.now(),
-        "receiverID": providerId,
-        "senderID": userId,
+        "receiverID": notMeId,
+        "senderID": meId,
         "text": message.text,
       });
 
       message.clear();
       notifyListeners();
     }
-  }
-
-  @override
-  Future<ChatTokenProviderModel?> futureToRun() async {
-    return await getUserFireStoreTokenAndProviderChatId();
   }
 }
