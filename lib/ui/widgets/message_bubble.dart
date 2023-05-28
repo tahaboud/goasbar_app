f8ee0987c32e88916c82475a59114b13ab693bde
Abdeldjalil Chougui <abdeldjalil.di.chougui@gmail.com>
2023-05-28 11:50:00 +0100

    commit message_bubble.dart

--- lib/ui/widgets/message_bubble.dart
+++ lib/ui/widgets/message_bubble.dart
@@ -7,29 +7,34 @@ class MessageBubble extends StatelessWidget {
   const MessageBubble({
     Key? key,
     this.message,
-    this.isSender,
+    this.isReciever,
   }) : super(key: key);
   final String? message;
-  final bool? isSender;
+  final bool? isReciever;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: screenWidthPercentage(context, percentage: 0.8),
-      child: Text(
-        message!,
-        overflow: TextOverflow.ellipsis,
-        style: TextStyle(color: isSender! ? Colors.black : Colors.white),
-        textAlign: isSender! ? TextAlign.left : TextAlign.right,
-      ).padding(horizontal: 20, vertical: 10),
-    ).decorated(
-      color: isSender! ? kAgencyColor : kMainColor1,
-      borderRadius: BorderRadius.only(
-        topRight: isSender! ? const Radius.circular(20) : const Radius.circular(5),
-        bottomRight: isSender! ? const Radius.circular(20) : const Radius.circular(5),
-        topLeft: isSender! ? const Radius.circular(5) : const Radius.circular(20),
-        bottomLeft: isSender! ? const Radius.circular(5) : const Radius.circular(20),
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        SizedBox(
+          // width: screenWidthPercentage(context, percentage: 0.8),
+          child: Text(
+            message!,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(color: isReciever! ? Colors.black : Colors.white),
+            textAlign: isReciever! ? TextAlign.left : TextAlign.right,
+          ).padding(horizontal: 20, vertical: 10),
+        ).decorated(
+          color: isReciever! ? kAgencyColor : kMainColor1,
+          borderRadius: BorderRadius.only(
+            topRight: isReciever! ? const Radius.circular(20) : const Radius.circular(5),
+            bottomRight: isReciever! ? const Radius.circular(20) : const Radius.circular(5),
+            topLeft: isReciever! ? const Radius.circular(5) : const Radius.circular(20),
+            bottomLeft: isReciever! ? const Radius.circular(5) : const Radius.circular(20),
+          ),
+        ),
+      ],
     );
   }
 }
